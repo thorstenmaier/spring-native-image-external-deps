96963f5852aeec87d0c600d55b2a7640ee41ec93
Thorsten Maier <Thorsten.Maier@oio.de>
2021-04-06 13:59:41 +0200

    Added jasypt dependency and some demo code -> Running image is failing

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.jasypt.util.text.AES256TextEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		AES256TextEncryptor textEncryptor = new AES256TextEncryptor();
+		textEncryptor.setPassword("pass");
+		String myEncryptedText = textEncryptor.encrypt("text");
+		String plainText = textEncryptor.decrypt(myEncryptedText);
+		System.out.println(plainText);
 	}
 
 }
